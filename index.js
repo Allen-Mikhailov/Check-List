ee986ad19d8e320e2e731c8030df136238767197
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2022-04-15 14:51:13 -0500

    working reply

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
 require('dotenv').config()
-const Discord = require('discord.js'); //import discord.js
 
-console.log(process.env.TEST)
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const token = process.env.CLIENT_TOKEN
+const clientId = process.env.CLIENT_ID
+const guildId = process.env.GUILD_ID
+const { Client, Intents } = require("discord.js")
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
+
+console.log(token)
+client.login(token);
+
+var rl = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.on("SIGINT", function () {
+    console.log("Exit")
+    client.destroy()
+    process.exit()
+  });
--- readMe.md
+++ readMe.md
@@ -1,11 +1,5 @@
 # Discord Checklist 
 
-## Main Dependencies
-- Nodejs
+## Set Up
+- node version 16.14.2 or up
 
-### Nodejs Dependencies
-- discord.js : 13.6.0
-- dotenv : 16.0.0
-
-## Important
-- Input your token with a .env file in this dir as CLIENT_TOKEN="YOUR_TOKEN_HERE"
